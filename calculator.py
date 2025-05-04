548124516a2ab39e05fffcfd63842461312b8fd5
PadarbindaPuhan <Padarbinda.Puhan@gmail.com>
2025-05-04 23:16:25 +0530

    added feature for multiplication

--- calculator.py
+++ calculator.py
@@ -4,9 +4,13 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+def multiply(x,y):
+    return x*y
+
 print("Select operation:")
 print("1. Add")
 print("2. Subtract")
+print("3. Multiply")
 
 choice = input("Enter choice (1/2/3/4): ")
 
@@ -17,5 +21,7 @@ if choice == '1':
     print("Result:", add(num1, num2))
 elif choice == '2':
     print("Result:", subtract(num1, num2))
+elif choice == '3':
+    print("Result:", multiply(num1, num2))
 else:
     print("Invalid input")
