eb4ba5821dadac660bbb0203a90cd2732e60064f
Dilip <dilip.samanta@gmail.com>
2025-05-05 12:46:03 +0530

    adding square

--- calculator.py
+++ calculator.py
@@ -12,13 +12,16 @@ def division(x,y):
         return "Can not divide by zero!"
     return x/y
 
+def square(x):
+    return x*x
+
 print("Select operation:")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Diision")
 
-choice = input("Enter choice (1/2/3/4): ")
+choice = input("Enter choice (1/2/3/4/5): ")
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
@@ -31,5 +34,7 @@ elif choice == '3':
     print("Result:", multiply(num1, num2))
 elif choice == '4':
     print("Result:", division(num1, num2))
+elif choice == "5":
+    print("Result:", square(num1))
 else:
     print("Invalid input")
